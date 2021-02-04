f143ef9958ef1e9066f220ceb11b226c4d6fcc63
Idris HAMMI <hammi.idris@gmail.com>
2021-02-04 15:56:44 +0100

    removed action logic js file, will build game in app_script

--- app_script.js
+++ app_script.js
@@ -12,5 +12,17 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
   document.getElementById('dice1').style.display = 'block';
   document.getElementById('dice2').style.display = 'block';
   document.getElementById('dice1').src = 'dice-' + dice1 + '.png';
+  document.getElementById('dice2').src = 'dice-' + dice2 + '.png';
+
+  if(dice1 === 6 && prevDiceRoll === 6) {
+    scores[activePlayer] = 0;
+    document.querySelector('#score-' + activePlayer).textContent = '0';
+    nextPlayer();
+  } else if (dice1 !== 1 && dice2 !== 1) {
+    roundScore += dice1 + dice2;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+      nextPlayer();
+  }
 })
 
